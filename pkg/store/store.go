3a353d77a4e9a05381f016dd039f2e859d622c10
Peter Udeh <94682093+Udehlee@users.noreply.github.com>
2024-08-14 19:16:43 +0100

    Update store.go

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -6,16 +6,6 @@ import (
 	"github.com/Udehlee/Task-Management/pkg/models"
 )
 
-type Store interface {
-	SaveUser(user models.User) error
-	UserByEmail(email string) (models.User, error)
-
-	GetAllUser() ([]models.User, error)
-	GetUserById(id int) (models.User, error)
-
-	InsertTask(task models.Task) error
-	UpdateTask(task models.Task) error
-}
 
 type PgConn struct {
 	Conn *sql.DB
