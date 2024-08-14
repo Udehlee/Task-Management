0390470085c85ccf9784f2fd5b362ed8cedfb993
Peter Udeh <94682093+Udehlee@users.noreply.github.com>
2024-08-14 19:18:16 +0100

    Update api.go

--- api/handler/api.go
+++ api/handler/api.go
@@ -7,18 +7,31 @@ import (
 	"github.com/Udehlee/Task-Management/pkg/store"
 )
 
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Udehlee/Task-Management/pkg/service"
+	"github.com/Udehlee/Task-Management/pkg/store"
+)
+
 type Handler struct {
-	Service service.Service
+	Service *service.Service
 }
 
-func NewHandler(db store.PgConn) Handler {
-
-	return Handler{
-		Service: service.Service{
-			Store: db,
-		},
+func NewHandler(db store.PgConn) *Handler {
+	svc := service.NewService(db)
+	return &Handler{
+		Service: svc,
 	}
 }
+
+func (h Handler) Index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome Home"))
+
+}
+
 
 func (h Handler) Index(w http.ResponseWriter, r *http.Request) {
 
